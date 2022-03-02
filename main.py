02808a808ab248a85b5736d4e3f16e71290143e3
abubakar <mabubakarbssg@gmail.com>
2022-03-02 02:03:49 -0500

    in dictinary, name and age

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-# Define a dictionary
-customers = {'06753':'Mehzabin Afroze','02457':'Md. Ali',
-'02834':'Mosarof Ahmed','05623':'Mila Hasan', '07895':'Yaqub Ali'}
+details = {'Name': "Bob", 'Age' :28}
 
-# Append a new data
-customers['05634'] = 'Mehboba Ferdous'
-
-print("The customer names are:")
-# Print the values of the dictionary
-for customer in customers:
-    print(customers[customer])
-
-# Take customer ID as input to search
-name = input("Enter customer ID:")
-
-# Search the ID in the dictionary
-for customer in customers:
-    if customer == name:
-        print(customers[customer])
-        break
+with open('file.txt','w') as data:
+	data.write(str(details))
